645945d9e86f4432dd2e3a9806a5533ff9e73f75
NyckdeVries21 <michaelwitkamp16@gmail.com>
2026-02-03 12:41:25 +0100

    feat: car movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,25 +3,38 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float Speed;
-    private Vector3 Movement;
+    public float moveSpeed; // 10
+    public float rotateSpeed; // 25
+
+    private float moveInput;
+    private float rotateInput;
     private Controls Controls;
+
+
 
     private Rigidbody rb;
     private void Awake()
     {
         Controls = new Controls();
-        Controls.Player.Enable();
+        Controls.Drive.Enable();
 
-        Controls.Player.Move.performed += OnMove;
-        Controls.Player.Move.canceled += OnMove;
+        Controls.Drive.Drive.performed += OnMove;
+        Controls.Drive.Drive.canceled += OnMove;
 
         rb = GetComponentInChildren<Rigidbody>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        Movement.x = context.ReadValue<Vector2>().x;
-        Movement.z = context.ReadValue<Vector2>().y;
+        Vector2 input = context.ReadValue<Vector2>();
+        moveInput = input.y;      // forward/back
+        rotateInput = input.x;    // left/right
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveInput * moveSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.up, rotateInput * rotateSpeed * Time.deltaTime);
     }
 }
